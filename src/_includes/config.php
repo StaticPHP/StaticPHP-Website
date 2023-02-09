5e2ade321482d165db754d38468cf8fd54235155
StaticPHP <contact@staticphp.io>
2023-02-09 04:03:39 +0000

    Changed font to Poppins stored locally, and moved w3.css to local storage.

--- src/_includes/config.php
+++ src/_includes/config.php
@@ -5,4 +5,6 @@ $site_tagline = "A Static Site Generator written in PHP. It turns source PHP fil
 $site_author = "David Hunter";
 $site_author_url = "https://davidhunter.scot";
 
-$stylesheets[] = "https://w3css.staticly.ict.rocks/4.15/w3.css";
+$stylesheets[] = "/assets/w3css/4.15/w3.css";
+$stylesheets[] = "/assets/webfonts/poppins/poppins.css";
+$stylesheets[] = "/assets/css/custom.css";
