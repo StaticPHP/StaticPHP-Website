0ff6bdfa7bf845e652285f67221a71680a85f9f0
StaticPHP <contact@staticphp.io>
2023-03-26 18:15:29 +0100

    Fixed page titles not working

--- src/_includes/base.php
+++ src/_includes/base.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-        <title><?php if( isset( $page_title ) && $page_title ) $page_title . ' - '; ?><?php echo $site_title; ?><?php if( ! isset( $page_title ) || $page_title ) echo ' - ' . $site_tagline; ?></title>
+        <title><?php if( isset( $page_title ) && $page_title ) echo $page_title . ' - '; ?><?php echo $site_title; ?><?php if( ! isset( $page_title ) || ! $page_title ) echo ' - ' . $site_tagline; ?></title>
 
         <?php include __DIR__ . DIRECTORY_SEPARATOR . 'stylesheets.php'; ?>
         
