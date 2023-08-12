72a7d50a9d1f2f70f595bb9f5c3e2c01403e5c3a
StaticPHP <contact@staticphp.io>
2023-08-12 21:05:23 +0100

    Added line to landing page about new support for HTML files

--- src/index.php
+++ src/index.php
@@ -6,6 +6,7 @@ layout: src/_includes/base.php
 <p><img src="/assets/images/StaticPHP-Promo1.png" alt="Static PHP Promo Image" class="w3-image"></p>
 <h2>Welcome to --- metadata.site_title ---!</h2>
 <p>--- metadata.site_tagline ---</p>
+<p><b>NEW:</b> Now supports <a href="https://docs.staticphp.io/html-files" target="_blank">HTML files</a>!</p>
 <p><a href="https://raw.githubusercontent.com/DavidHunterScot/StaticPHP/master/StaticPHP-Launcher.php" target="_blank">Download Launcher</a> or <a href="https://raw.githubusercontent.com/DavidHunterScot/StaticPHP/master/StaticPHP.php" target="_blank">Download --- metadata.site_title ---</a></p>
 <p><a href="https://github.com/DavidHunterScot/StaticPHP" target="_blank">Source Code on GitHub</a></p>
 
