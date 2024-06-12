a5c3bc33cefda9135df3cea09c957d1d1166a624
StaticPHP <contact@staticphp.io>
2024-06-12 01:36:35 +0100

    Made changes to the download page, most noteably the addition of GitLab.

--- src/download.php
+++ src/download.php
@@ -7,12 +7,38 @@ layout: src/_includes/base.php
 <div class="hero">
     <div class="container">
         <h2>Download</h2>
-        <p>Get your hands on it, and start using it.</p>
+        
+        <p>To get StaticPHP for your own project, you have two options.</p>
+
+
+        <h3>Download The Launcher</h3>
+        
+        <p>The StaticPHP Launcher makes it easy to save your build configuration and stay up to date with the latest developments in StaticPHP.</p>
 
         <div class="call2action">
-            <a href="https://raw.githubusercontent.com/DavidHunterScot/StaticPHP/master/StaticPHP-Launcher.php" target="_blank" class="button highlight">Launcher</a>
+            <a href="http://raw.githubusercontent.com/DavidHunterScot/StaticPHP/master/StaticPHP-Launcher.php" target="_blank" class="button highlight">StaticPHP-Launcher.php on GitHub</a>
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="https://raw.githubusercontent.com/DavidHunterScot/StaticPHP/master/StaticPHP.php" target="_blank" class="button">StaticPHP</a>
+            <a href="http://gitlab.com/StaticPHP/StaticPHP/-/raw/master/StaticPHP-Launcher.php" target="_blank" class="button">StaticPHP-Launcher.php on GitLab</a>
         </div>
+
+        
+        <p>&nbsp;</p>
+
+
+        <h3>Download StaticPHP</h3>
+
+        <p>Alternatively, you can download the actual StaticPHP class file for use with the commandline or your own launcher script.</p>
+
+        <div class="call2action">
+            <a href="http://raw.githubusercontent.com/StaticPHP/StaticPHP/master/StaticPHP.php" target="_blank" class="button highlight">StaticPHP.php on GitHub</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="http://gitlab.com/StaticPHP/StaticPHP/-/raw/master/StaticPHP.php" target="_blank" class="button">StaticPHP.php on GitLab</a>
+        </div>
+
+        <p>&nbsp;</p>
+
+        <hr>
+
+        <p>To learn more about the above methods, check out the <a href="/docs/getting-started">Getting Started</a> guide.</p>
     </div>
 </div>
